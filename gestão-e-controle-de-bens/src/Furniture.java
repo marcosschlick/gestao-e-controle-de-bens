461d5303df6b1cfee4a8b0edc155848f564795c3
marcosschlick <marcosschlick@gmail.com>
2024-11-13 20:31:52 -0300

    java file created

---
+++ gestão-e-controle-de-bens/src/Furniture.java
@@ -0,0 +1,13 @@
+import java.sql.Date;
+
+public class Furniture {
+	
+	private int code;
+	private String description;
+	private Type type;
+	private Date date;
+	private Double value;
+	private State state;
+	
+
+}
